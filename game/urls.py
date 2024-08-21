f7fa3135c3dbec90f10013c1647451570fcd050e
mucahitcakmak <mucoox18@gmail.com>
2024-08-21 16:21:11 +0300

    Sayfa yönlendirmelerinin temelleri atıldı ve bu sefer güzel çalışıyor. fakat 404 urlleri için ne yapılacak belirlenmedi.

--- game/urls.py
+++ game/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
 	path("", views.MainPage, name="MainPage"),
--- pong/urls.py
+++ pong/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.shortcuts import redirect
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: redirect('/anasayfa', permanent=True)),
     path('', include("game.urls")),
 ]
